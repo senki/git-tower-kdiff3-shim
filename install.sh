32ffec77c26da2db8af78ad4cd78a2f7d3d65650
Csaba Maulis <csaba.maulis@gmail.com>
2018-09-21 16:16:47 +0800

    Update install to Tower 3

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-mkdir ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
-cp CompareTools.plist ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
-cp -R CompareScripts/* ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
+mkdir ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
+cp CompareTools.plist ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
+cp -R CompareScripts/* ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
